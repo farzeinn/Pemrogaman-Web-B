0249803fc043a8ab07ad2841f884e5b6d0edae29
farzeinn <71867429+farzeinn@users.noreply.github.com>
2021-03-10 20:20:47 +0700

    10/03/2021

---
+++ pertemuan_1/dasar.php
@@ -0,0 +1,19 @@
+<?php 
+mhs = [
+    [
+        'nama' => 'Farhan'
+        'NIM' => '192410101073'
+        'usia' => '20'
+    ]
+]
+?>
+
+<?php for ($i=0; $i <4 ; $i++) { 
+    echo "hai <br>";
+}
+
+
+<?php for ($i=0; $i <5 ; $i++) { 
+    echo "<h1>Hello World</h1>";
+}
+?>
